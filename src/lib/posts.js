f330188723c3046d35b007ba2d4700056676894d
Deni Supriyatna <denisupriyatna01@gmail.com>
2024-01-30 09:15:24 +0700

    implement getStaticProps

--- src/lib/posts.js
+++ src/lib/posts.js
@@ -1,58 +1,67 @@
-import fs from 'fs';
-import path from 'path';
-import matter from 'gray-matter';
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
 
-const postDirectroy = path.join(process.cwd(), 'posts');
-
+const postDirectroy = path.join(process.cwd(), "posts");
 
 export function getSortedPostsData() {
-    // get file names under /posts
-    const fileNames = fs.readdirSync(postDirectroy);
-    const allPostData = fileNames.map((fileName) => {
-        // remove ".md from file name to get id"
-        const id = fileName.replace(/\.md$/, '');
+  // get file names under /posts
+  const fileNames = fs.readdirSync(postDirectroy);
+  const allPostData = fileNames.map((fileName) => {
+    // remove ".md from file name to get id"
+    const id = fileName.replace(/\.md$/, "");
 
-        // Read markdown file as string
-        const fullPath = path.join(postDirectroy, fileName);
-        const fileContent = fs.readFileSync(fullPath, 'utf8');
+    // Read markdown file as string
+    const fullPath = path.join(postDirectroy, fileName);
+    const fileContent = fs.readFileSync(fullPath, "utf8");
 
-        // use gray matter to parse the post metadata section
-        const matterResult = matter(fileContent);
+    // use gray matter to parse the post metadata section
+    const matterResult = matter(fileContent);
 
-        // combine the data with the id
-        return {
-            id,
-            ...matterResult.data,
-        };
-    });
+    // combine the data with the id
+    return {
+      id,
+      ...matterResult.data,
+    };
+  });
 
-    // sort posts by date
-    return allPostData.sort((a, b) => {
-        if (a.date < b.date) {
-            return 1;
-        } else {
-            return -1;
-        }
-    });
+  // sort posts by date
+  return allPostData.sort((a, b) => {
+    if (a.date < b.date) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
 }
 
-
 export function getAllPostIds() {
-    const fileNames = fs.readdirSync(postDirectroy);
+  const fileNames = fs.readdirSync(postDirectroy);
 
-    let id = fileNames.map((fileName) => {
-        return {
-            params: {
-                id: fileName.replace(/\.md$/, '')
-            },
-        };
-    });
+  let id = fileNames.map((fileName) => {
+    return {
+      params: {
+        id: fileName.replace(/\.md$/, ""),
+      },
+    };
+  });
 
-    console.log(id);
-    return id;
+  console.log(`all post `);
+  console.log(id);
+  return id;
 }
 
 export function getPostData(id) {
-    const fullPath = path.join(postDirectroy, `${id}.md5`);
-    const fullContents = fs.readFileSync(fullPath, 'utf8');
+  const fullPath = path.join(postDirectroy, `${id}.md`);
+  console.log(fullPath);
+  const fileContents = fs.readFileSync(fullPath, "utf8");
+
+  // use gray-matter to parse the post metadata section
+  const matterResult = matter(fileContents);
+
+  // combine the data and the id
+  return {
+    id,
+    ...matterResult.data,
+  };
 }
--- src/pages/post/[id].js
+++ src/pages/post/[id].js
@@ -1,19 +1,36 @@
 import Layout from "@/components/layout";
-import { getAllPostIds } from "@/lib/posts";
+import { getAllPostIds, getPostData } from "@/lib/posts";
 
-export default function Post() {
-    console.log(id);
-    return (
-        <Layout> ... </Layout>
-    );
+export default function Post({ postData }) {
+  // console.log(id);
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+    </Layout>
+  );
 }
 
-
 export async function getStaticPaths() {
-    const paths = getAllPostIds();
+  // return a list  of possible value for id
+  const paths = getAllPostIds();
 
-    return {
-        paths,
-        fallback: false
-    }
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  // fetch nesesary data for the blog post using param id
+  const postData = getPostData(params.id);
+  console.log(params);
+  return {
+    props: {
+      postData,
+    },
+  };
 }
